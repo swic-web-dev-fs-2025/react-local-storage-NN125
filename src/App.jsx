ab4df08b242ff7efad71a754e9227a527182c4c9
NN125 <nicolas.nolden@student.swic.edu>
2025-11-07 23:03:22 -0600

    create button states

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import "./App.css";
 
 export default function App() {
 
+const [buttonStates, setButtonStates] = useState({
+  button1: false,
+  button2: false,
+  button3: false
+});
   return (
     <>
       <h1>Button State</h1>
