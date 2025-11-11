f74cde55ec25d542e41d2d5f222bdd8914352db6
NN125 <nicolas.nolden@student.swic.edu>
2025-11-11 11:45:04 -0600

    create useCount

---
+++ src/customHookExample/useCount.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export function useCount(initialValue = 0) {
+  const [count, setCount] = useState(initialValue);
+
+  const increment = () => setCount((c) => c + 1);
+  const decrement = () => setCount((c) => c - 1);
+
+  return { count, increment, decrement };
+}
